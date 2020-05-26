56c2ec838a1e86580e7410a8355e63e9fb68e985
Greg-Hamel <hamegreg@gmail.com>
2020-05-26 09:57:45 -0400

    feat: add a 404 - page not found
    added a new component "NoMatchPage" and modified the App routing with a Switch component which will
    break when a match is made.

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { 
   BrowserRouter as Router,
   Route,
+  Switch,
 } from 'react-router-dom'
 import './App.css'
 
@@ -14,20 +15,24 @@ import PasswordForgetPage from '../PasswordForget';
 import AccountPage from '../Account';
 import RequestPage from '../Request';
 import { PrivacyPolicy, ToS } from '../Policies';
+import NoMatchPage from '../NoMatchPage'
 
 import * as ROUTES from '../../constants/routes';
 
 const App = () => (
   <Router>
     <div className="app">
-      <Route path={ROUTES.LANDING} exact component={LandingPage} />
-      <Route path={ROUTES.REQUEST} component={RequestPage} />
-      <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
-      <Route path={ROUTES.SIGN_IN} component={SignInPage} />
-      <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
-      <Route path={ROUTES.ACCOUNT} component={AccountPage} />
-      <Route path={ROUTES.PRIVACY_POLICY} component={PrivacyPolicy} />
-      <Route path={ROUTES.TERMS_OF_SERVICE} component={ToS} />
+      <Switch>
+        <Route exact path={ROUTES.LANDING} exact component={LandingPage} />
+        <Route path={ROUTES.REQUEST} component={RequestPage} />
+        <Route exact path={ROUTES.SIGN_UP} component={SignUpPage} />
+        <Route exact path={ROUTES.SIGN_IN} component={SignInPage} />
+        <Route exact path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
+        <Route exact path={ROUTES.ACCOUNT} component={AccountPage} />
+        <Route exact path={ROUTES.PRIVACY_POLICY} component={PrivacyPolicy} />
+        <Route exact path={ROUTES.TERMS_OF_SERVICE} component={ToS} />
+        <Route component={NoMatchPage} />
+      </Switch>
     </div>
   </Router>
 )
