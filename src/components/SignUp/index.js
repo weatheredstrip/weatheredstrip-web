b60d7a2f7030847e8dd00f46ad8652fa1a30c407
Greg-Hamel <hamegreg@gmail.com>
2020-05-26 15:14:10 -0400

    refactor(signup): rewrite for responsive UI and Buttons

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Link as RouterLink, withRouter } from 'react-router-dom';
 import { compose } from 'recompose';
-import { TextInput, Heading, Button, Spinner, Pane, Text, Checkbox, Link, Card } from 'evergreen-ui'
+import { Alert, TextInput, Heading, Button, Spinner, Pane, Text, Checkbox, Link, Card } from 'evergreen-ui'
 
 import { withFirebase } from '../Firebase';
 import { ToSDialogLink, PrivacyPolicyDialogLink, ToSDialog } from '../Policies'
@@ -13,8 +13,6 @@ const SignUpPage = () => (
   </div>
 );
 
-const LoadingContext = React.createContext({ isLoading: false });
-
 const INITIAL_STATE = {
   firstName: '',
   lastName: '',
@@ -24,6 +22,7 @@ const INITIAL_STATE = {
   error: null,
   readBoth: false,
   isLoading: false,
+  isComplete: false,
 };
 
 class SignUpFormBase extends Component {
@@ -34,6 +33,7 @@ class SignUpFormBase extends Component {
 
   onSubmit = event => {
     const { firstName, lastName, email, passwordOne } = this.state;
+    this.setState({ isLoading: true })
     this.props.firebase
       .doCreateUserWithEmailAndPassword(email, passwordOne)
       .then(authUser => {
@@ -55,11 +55,10 @@ class SignUpFormBase extends Component {
         return this.props.firebase.doSendEmailVerification()
       })
       .then(() => {
-        this.setState({ ...INITIAL_STATE });
-        this.props.history.push(ROUTES.LANDING);
+        this.setState({ ...INITIAL_STATE, isComplete: true });
       })
       .catch(error => {
-        this.setState({ error });
+        this.setState({ error, isLoading: false });
       });
     event.preventDefault();
   }
@@ -81,6 +80,8 @@ class SignUpFormBase extends Component {
       passwordTwo,
       error,
       readBoth,
+      isComplete,
+      isLoading
     } = this.state;
 
     const isInvalid =
@@ -92,8 +93,9 @@ class SignUpFormBase extends Component {
       !readBoth;
 
     return (
-      <LoadingContext.Provider value={{ isLoading: this.state.isLoading }}>
+      <>
         <Card
+          maxWidth="calc(100vw - 16px * 2)"
           width={560}
           background="tint1"
           elevation={2}
@@ -111,69 +113,95 @@ class SignUpFormBase extends Component {
               alignItems="center"
               flexDirection="column"
             >
-              <Text >
-                Accounts will provide the ability to add presets in the future.
-              </Text>
-              <Text marginBottom={20}>
-                Option for feature update emails will also be provided.
-              </Text>
-              <TextInput
-                name="firstName"
-                value={firstName}
-                onChange={this.onChange}
-                type="text"
-                placeholder="First Name"
-                marginBottom={10}
-              />
-              <TextInput
-                name="lastName"
-                value={lastName}
-                onChange={this.onChange}
-                type="text"
-                placeholder="Last Name"
-                marginBottom={10}
-              />
-              <TextInput
-                name="email"
-                value={email}
-                onChange={this.onChange}
-                type="text"
-                placeholder="Email Address"
-                marginBottom={10}
-              />
-              <TextInput
-                name="passwordOne"
-                value={passwordOne}
-                onChange={this.onChange}
-                type="password"
-                placeholder="Password"
-                marginBottom={10}
-              />
-              <TextInput
-                name="passwordTwo"
-                value={passwordTwo}
-                onChange={this.onChange}
-                type="password"
-                placeholder="Confirm Password"
-                marginBottom={10}
-              />
-              <Pane
-                display="flex"
-                flexDirection="row"
-                alignItems="center"
-              >
-                <Checkbox
-                  marginRight={20} 
-                  name="readBoth"
-                  checked={readBoth}
-                  onChange={this.onCheck}
-                  label={
+              {error ? (
+                <Alert
+                  intent="danger"
+                  title={error.message}
+                  marginBottom={10}
+                />
+              ) : isComplete ? (
+                  <Alert
+                    intent="success"
+                    title="Welcome, abord!"
+                    marginBottom={10}
+                  >
+                    You'll need to verify your e-mail before having access to further account settings. Check your inbox, we'll be right here when you get back.
+                  </Alert>
+              ) : (
+                <>
                   <Text>
-                      I have read the <ToSDialogLink /> and <PrivacyPolicyDialogLink/>
+                    Accounts will provide the ability to add presets in the future.
                   </Text>
-                }/>
-              </Pane>
-              {error && <p className="login-form-error">{error.message}</p>}
+                  <Text marginBottom={20}>
+                    Option for feature update emails will also be provided.
+                  </Text>
+                </>
+              )}
+              {!isComplete && (
+                <>
+                  <TextInput
+                    name="firstName"
+                    value={firstName}
+                    onChange={this.onChange}
+                    type="text"
+                    placeholder="First Name"
+                    marginBottom={10}
+                    maxWidth="100%"
+                  />
+                  <TextInput
+                    name="lastName"
+                    value={lastName}
+                    onChange={this.onChange}
+                    type="text"
+                    placeholder="Last Name"
+                    marginBottom={10}
+                    maxWidth="100%"
+                  />
+                  <TextInput
+                    name="email"
+                    value={email}
+                    onChange={this.onChange}
+                    type="text"
+                    placeholder="Email Address"
+                    marginBottom={10}
+                    maxWidth="100%"
+                  />
+                  <TextInput
+                    name="passwordOne"
+                    value={passwordOne}
+                    onChange={this.onChange}
+                    type="password"
+                    placeholder="Password"
+                    marginBottom={10}
+                    maxWidth="100%"
+                  />
+                  <TextInput
+                    name="passwordTwo"
+                    value={passwordTwo}
+                    onChange={this.onChange}
+                    type="password"
+                    placeholder="Confirm Password"
+                    marginBottom={10}
+                    maxWidth="100%"
+                  />
+                  <Pane
+                    display="flex"
+                    flexDirection="row"
+                    alignItems="center"
+                  >
+                    <Checkbox
+                      marginRight={20}
+                      name="readBoth"
+                      checked={readBoth}
+                      onChange={this.onCheck}
+                      label={
+                        <Text>
+                          I have read the <ToSDialogLink /> and <PrivacyPolicyDialogLink />
+                        </Text>
+                      } />
+                  </Pane>
+                </>
+              )}
             </Pane>
             <Pane
               display="flex"
@@ -182,23 +210,28 @@ class SignUpFormBase extends Component {
               justifyContent="flex-end"
             >
               <Button
-              marginRight={20}
+              marginRight={!isComplete ? 20 : null}
+              appearance={!isComplete ? null : "primary"}
+              intent={!isComplete ? null : "success"}
               onClick={() => this.props.history.goBack()}
-              >Back</Button>
-              <Button
-                appearance="primary"
-                disabled={isInvalid || this.state.isLoading}
-                className="login-form-button"
-                type="submit"
-                onClick={this.onSubmit}
               >
-                {this.state.isLoading ? <Spinner size={12} /> : null}
-                {this.state.isLoading ? "Loading..." : "Sign Up"}
+                Back
               </Button>
+              {!isComplete && (
+                <Button
+                  appearance="primary"
+                  disabled={isInvalid}
+                  type="submit"
+                  onClick={this.onSubmit}
+                  isLoading={isLoading}
+                >
+                  {isLoading ? "Loading..." : "Sign Up"}
+                </Button>
+              )}
             </Pane>
           </form>
         </Card>
-      </LoadingContext.Provider>
+      </>
     );
   }
 }
