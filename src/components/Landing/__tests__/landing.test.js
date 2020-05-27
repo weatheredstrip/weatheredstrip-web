a0662a9331fc2de8206ee31ccdc42786424ff1ca
Greg-Hamel <hamegreg@gmail.com>
2020-05-27 16:08:58 -0400

    test(landing): add test for navigation

--- src/components/Landing/__tests__/landing.test.js
+++ src/components/Landing/__tests__/landing.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, cleanup } from '@testing-library/react'
+import { render, cleanup, fireEvent, screen } from '@testing-library/react'
 import { 
   BrowserRouter as Router
 } from 'react-router-dom'
@@ -9,6 +9,31 @@ afterEach(cleanup)
 
 it('landing renders properly', () => {
   const { container } = render(<Router><Landing /></Router>)
+
+  expect(container).toMatchSnapshot()
+})
+
+it('has navigation button', () => {
+  const { container } = render(<Router><Landing/></Router>)
   const landing = container.firstChild
-  expect(landing).toMatchSnapshot()
+  expect(landing.firstChild.nodeName).toEqual("BUTTON")
+})
+
+it('opens navigation when button is clicked', () => {
+  const { container } = render(<Router><Landing /></Router>)
+
+  const landing = container.firstChild
+
+  const button = landing.firstChild
+
+  fireEvent(
+    button,
+    new MouseEvent('click', {
+      bubbles: true,
+      cancelable: false,
+    })
+  )
+
+  const menu = screen.getByRole("menu");
+  expect(menu).not.toBeNull();
 })
